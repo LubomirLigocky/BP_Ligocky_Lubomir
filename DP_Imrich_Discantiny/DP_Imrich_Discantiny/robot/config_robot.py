c836f200dd3f85fb0cca238f1a037b347293e0a8
LubomirLigocky <145336943+LubomirLigocky@users.noreply.github.com>
2026-05-11 01:14:33 +0200

    Pridanie adresára s prácou DP_Imrich_Discantiny

---
+++ DP_Imrich_Discantiny/DP_Imrich_Discantiny/robot/config_robot.py
@@ -0,0 +1,9 @@
+IP_SERVER = '127.0.0.1'
+PORT_SERVER = 1234
+
+IS_PHYSICAL = False
+
+IP_NAO = "192.168.71.194"
+IP_NAO_VIRTUAL = '127.0.0.1'
+
+NAOQI_PATH = 'export PYTHONPATH=${PYTHONPATH}:/usr/local/lib/pynaoqi/lib/python2.7/site-packages'
